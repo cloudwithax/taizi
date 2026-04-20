e0c9fe2fbe04831a24df860a779d76c0b0ae6269
cloudwithax <heyimnick14@gmail.com>
2026-04-20 14:25:46 -0400

    Bump version to 1.0.4-release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.taizi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0-release"
+        versionCode = 4
+        versionName = "1.0.4-release"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
