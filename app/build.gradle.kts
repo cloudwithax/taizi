6938f238d77a739389e77f39d75bfe72a4632ee9
cloudwithax <heyimnick14@gmail.com>
2026-04-22 14:40:48 -0400

    chore: bump version to 1.7.0 (versionCode 15)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.taizi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.6.2"
+        versionCode = 15
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
