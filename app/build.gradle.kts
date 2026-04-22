755abb45a142e49f7cd40b383c7fdc823008f893
cloudwithax <heyimnick14@gmail.com>
2026-04-22 15:59:25 -0400

    chore: bump version to 1.8.4 (versionCode 20)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.taizi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.8.3"
+        versionCode = 20
+        versionName = "1.8.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
