7668522527605d4a522bcb33ef397f27385aad23
cloudwithax <heyimnick14@gmail.com>
2026-05-14 16:04:28 -0400

    chore: bump version to 1.9.5 (versionCode 29)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.taizi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 27
-        versionName = "1.9.4"
+        versionCode = 29
+        versionName = "1.9.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
