bb9cc40fb1f85ca2ee5d2e35365992a1cdf84733
cloudwithax <heyimnick14@gmail.com>
2026-09-20 11:32:43 -0400

    chore: bump version to 1.10.13 (versionCode 46)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.taizi"
         minSdk = 26
         targetSdk = 35
-        versionCode = 45
-        versionName = "1.10.12"
+        versionCode = 46
+        versionName = "1.10.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
